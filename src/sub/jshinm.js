4b41e5a8bdaba9c4fc3b2c1e38e9697471cae921
Jong Shin <jshin.m@gmail.com>
2023-02-12 04:53:41 -0500

    Create a component for jshinm redirect

---
+++ src/sub/jshinm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "../App.css"
+
+export default class jshinm extends Component{
+    render() {
+        return (
+            <div class="embed">
+                <iframe src="https://jshinm.github.io/" title="embedded page" class='embed' />
+            </div>
+        )
+    }
+}
