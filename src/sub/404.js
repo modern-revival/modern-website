763a9da90995b68d742c3fb7bbbf22d51057b6df
Jong Shin <jshin.m@gmail.com>
2023-02-12 04:53:17 -0500

    Create a component for 404

---
+++ src/sub/404.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import wip from '../img/cone.png';
+import underconst from '../img/underconst.png';
+
+export default class jshinm extends Component{
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+                <img src={wip} className="App-logo" alt="logo" />
+                <p>
+                <img src={underconst} alt="construction" width="30%"/>
+                </p>
+            </header>
+            </div>
+        )
+    }
+}
